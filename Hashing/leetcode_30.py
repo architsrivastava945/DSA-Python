aec6a8971a61c521ddc63ec10b2ee51a992f0e62
architsrivastava945 <architsrivastava945@gmail.com>
2026-07-24 17:34:59 +0530

    add incomplete sol of Substring with Concatenation of All Words

---
+++ Hashing/leetcode_30.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findSubstring(self, s: str, words: list[str]) -> list[int]:
+        
+
+s = input('s = ')
+words = input('words = ').split()
+object = Solution()
+ans = object.findSubstring(s, words)
+print(ans)
