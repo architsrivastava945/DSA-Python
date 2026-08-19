27a3bdc4446b891a64e8ee7b8efa68c4acb8fa66
architsrivastava945 <architsrivastava945@gmail.com>
2026-08-19 16:50:44 +0530

    min window substr sliding win completed

--- ArrayStringTwoPointSlideWin/leetcode_76.py
+++ ArrayStringTwoPointSlideWin/leetcode_76.py
@@ -1,21 +1,30 @@
 # Minimum Window Substring
+# abc end = 2, start = 0, i = 0, j= 2, ab, abc
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
+    def minWindow(self, s: str, t: str) -> str:  
+        if len(t) > len(s):
+            return ""
         start, end = 0, 0
         result = ""
+        dub = []
+        tempT = list(t)
         while(end < len(s)):
-            tempT = list(t)
-            dub = []
-            nextItr = end
-            for j in range(nextItr, len(s)):
+            for j in range(end, len(s)):
                 if s[j] in tempT:
                     tempT.remove(s[j])
                     if len(tempT) == 0:
-                        end = j
+                        end = j + 1
                         break
                 elif s[j] in t:
                     dub.append(s[j])
-            for i in range(nextItr, len(s)):
+                    if len(tempT) == 0:
+                        end = j + 1
+                        break
+
+            if len(tempT) > 0:
+                break
+
+            for i in range(start, len(s)):
                 if s[i] not in t:
                     continue
                 elif s[i] in dub:
@@ -23,11 +32,13 @@ class Solution:
                 else:
                     start = i
                     break
-            if j-i+1 < len(result):
-                result = s[i:j+1]
-        return result
-        
 
+            if result == "" or end-start < len(result):
+                result = s[start : end]
+
+            tempT.append(s[start])
+            start += 1
+        return result        
 
 s = input()
 t = input()
