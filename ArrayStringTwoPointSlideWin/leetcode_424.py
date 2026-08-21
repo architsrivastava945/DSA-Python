86613fc196c5d27fc5da7f66407cc4e396517145
architsrivastava945 <architsrivastava945@gmail.com>
2026-08-21 16:59:30 +0530

    update soln long substr (incomplete)

--- ArrayStringTwoPointSlideWin/leetcode_424.py
+++ ArrayStringTwoPointSlideWin/leetcode_424.py
@@ -1,28 +1,35 @@
 # 424. Longest Repeating Character Replacement
+from collections import Counter
 class Solution:
     def calc_unmatch(self, tempS):
         if len(tempS) < 1:
-            return 0
-        tempS = list(tempS)
-        tempS.sort()
-        key = tempS[int(len(tempS)/2)]
-        unmatch = 0
-        for i in tempS:
-            if i != key:
-                unmatch += 1
-        return unmatch
+            return (0, "NA")
+        freq = Counter(tempS)
+        maxFreq = freq[tempS[0]]
+        maxKey = "NA"
+        for key, value in freq.items():
+            if value > maxFreq:
+                maxFreq = value
+                maxKey = key
+        return (len(tempS) - maxFreq, key)
+
 
     def characterReplacement(self, s: str, k: int) -> int:
         unmatch = 0
         maxLen = 0
-        i,j = 0,0
+        i,j = 0,k
+        (unmatch, key) = self.calc_unmatch(s[i:j])
         while j < len(s):
             if unmatch > k:
+                if s[i] != key:
+                    unmatch -= 1
                 i += 1
-                unmatch = self.calc_unmatch(s[i:j])
             else:
+                if s[j] != key:
+                    unmatch += 1
                 j += 1
-                unmatch = self.calc_unmatch(s[i:j])
+            if unmatch > ((j-i)/2):
+                (unmatch, key) = self.calc_unmatch(s[i:j])
             if unmatch <= k:
                 maxLen = max(maxLen, j-i)
         return maxLen
@@ -31,5 +38,8 @@ class Solution:
 s = input()
 k = int(input())
 Obj = Solution()
-result = Obj.characterReplacement(s, k)
+result = Obj.characterReplacement(s, k) 
 print(result)
+
+# Input: s = "AABABBA", k = 1
+# Output: 4
