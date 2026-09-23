f3bfc8e6de3ae260e09ab24feef1054585fb2e0a
architsrivastava945 <architsrivastava945@gmail.com>
2026-09-23 18:22:19 +0530

    add lcd in bst correct  soln

--- tree/LCD_bst.py
+++ tree/LCD_bst.py
@@ -5,3 +5,19 @@ def lcd_in_bst(root, p, q):
     left_travel = lcd_in_bst(root.left, p, q)
 
     return right_travel if right_travel else left_travel
+
+def lowest_common_ancestor_bst(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    curr = root
+    
+    while curr:
+        # If both p and q are smaller than curr, go left
+        if p.val < curr.val and q.val < curr.val:
+            curr = curr.left
+        # If both p and q are greater than curr, go right
+        elif p.val > curr.val and q.val > curr.val:
+            curr = curr.right
+        else:
+            # They have split! Current node is the LCA
+            return curr
+            
+    return None
